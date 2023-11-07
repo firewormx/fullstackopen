beebdc7a041cff57cff2bee4b2eb96df18d3d233
hotaru <huyuchen0810@gmail.com>
2023-11-07 19:29:31 +0900

    update 1.7

--- Part1/unicafe/unicafe/src/App.jsx
+++ Part1/unicafe/unicafe/src/App.jsx
@@ -6,16 +6,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0);
+
 
   const handleGoodincrement = ()=>{
     setGood(good + 1);
+    setTotal(total + 1);
+
   }
   const handleNeutralIncrement = () =>{
     setNeutral(neutral + 1)
+    setTotal(total + 1)
+
+
   }
   const handleBadIncrement = ()=>{
     setBad(bad + 1);
+    setTotal(total+1);
+
   }
+const sum = good *1 +neutral * 0 + bad *(-1);
+const average = sum / total;
+const positive = good / total * 100;
+
   return (<>
     <div>
       <h2>give feedback</h2>
@@ -28,6 +41,10 @@ const App = () => {
     <p>good {good}</p> 
     <p>neutral {neutral}</p>
     <p>bad {bad}</p> 
+    <p>all {total}</p>
+    <p>sum {sum}</p>
+    <p>average {average || ""}</p>
+    <p>positive {positive || ""}%</p>
     </div>
     </> )
 }
