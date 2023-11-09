ea36f08dfe7c359a8075ac30872d3a2814c61253
hotaru <huyuchen0810@gmail.com>
2023-11-09 16:14:00 +0900

    update 1.13-1.14

--- Part1/anexdotes/anexdotes/src/App.jsx
+++ Part1/anexdotes/anexdotes/src/App.jsx
@@ -1,7 +1,33 @@
 import { useState } from 'react'
 
+ const Button = (props) =>{
+  return (
+    <div>
+    <button onClick={props.handleClick}>{props.text}</button>
+    </div>
+  )
+ }
 
+ const Title = (props) => {
+  return   <h2>{props.text}</h2>
+ }
+
+ const Anecdotes = (props) =>{
+  return (<>
+  <div>{props.anecdote}</div>
+  <div>has {props.vote} votes</div>
+  </>
+  )
+ }
+
+ const MostVotes = (props) =>{
+  return <>
+  <div>{props.most}</div>
+<div>has {props.max} votes</div>
+  </>
+ }
 const App = () => {
+
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -11,19 +37,31 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-   
-  const  nextAnecdote = ()=>{
+  ];
+
+  const [voted, setVoted]= useState(Array(anecdotes.length).fill(0));
+  const [selected, setSelected] = useState(0);
+
+  const  handleNextAnecdote = ()=>{
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
+  const handleVoteAnecdote = () =>{
+    const newVotes = [...voted];
+    newVotes[selected] += 1;
+    setVoted(newVotes);
+  }
+
+  const max= Math.max(...voted);
+  const index = voted.indexOf(max);
 
   return (<>
-<button onClick={nextAnecdote}>next anecdote</button>
-    <div>
-      {anecdotes[selected]}
-    </div>
+  <Title text="Anecdote of the day"/>
+  <Anecdotes  anecdote={anecdotes[selected]} vote={voted[selected]}/>
+  <Button handleClick={handleVoteAnecdote} text="vote" />
+<Button handleClick={handleNextAnecdote} text="next anecdote"/>
+
+<Title text="Anecdote with most votes"/>
+<MostVotes most={anecdotes[index]} max={max}/>
 </>
   )
 }
