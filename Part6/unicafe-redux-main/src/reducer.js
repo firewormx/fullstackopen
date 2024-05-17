4ddbd0ecb1aa45561f4bf2147ddcf762af2f1640
hotaru <huyuchen0810@gmail.com>
2024-05-17 12:09:12 +0900

    update unicafe-redux

--- Part6/unicafe-redux-main/src/reducer.js
+++ Part6/unicafe-redux-main/src/reducer.js
@@ -9,12 +9,14 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return  {...state, good : state.good + 5}
+      return  {...state, good : state.good + 1}
     case 'OK':
-      return  {...state, ok: state.ok + 4}
+      return  {...state, ok: state.ok + 1}
     case 'BAD':
-      return {...state, bad: state.bad + 2}
+      return {...state, bad: state.bad + 1}
     case 'ZERO':
+      return initialState
+      case 'DO_NOTHTING':
       return initialState
     default: return state
   }
