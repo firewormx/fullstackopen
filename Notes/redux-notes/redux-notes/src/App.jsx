afa4609a9f146ded32649e882a3fc3288eccabf7
hotaru <huyuchen0810@gmail.com>
2024-05-20 11:04:03 +0900

    update redux-notes

--- Notes/redux-notes/redux-notes/src/App.jsx
+++ Notes/redux-notes/redux-notes/src/App.jsx
@@ -1,1 +1,64 @@
+import {createNote, toggleImportanceOf} from './reducers/noteReducer'
+import {useSelector, useDispatch} from 'react-redux'
 
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   payload: {
+//     content: 'the app state is in redux store',
+//     important: true,
+//     id: 1
+//   }
+// })
+
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   payload: {
+//     content: 'state changes are made with actions',
+//     important: false,
+//     id: 2
+//   }
+// })
+
+// store.dispatch({
+//   type: 'TOGGLE_IMPORTANCE',
+//   payload: {
+//     id: 2
+// }
+// })
+
+
+const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)//the fnc either searches for or selects data from the Redux store.
+  const importantNotes = useSelector(state => state.filter(note => note.important))
+
+  const addNote = (event) => {
+   event.preventDefault()
+   const content = event.target.note.value
+   event.target.note.value = ''
+   dispatch(createNote(content))
+  }
+  const toggleImportance = (id) =>{
+ dispatch(toggleImportanceOf(id))
+  }
+
+  return(
+    <div>
+      <form onSubmit={addNote}>
+        <input name= 'note'/>
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {notes.map(note=>
+          <li
+           key= {note.id}
+           onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+export default App
