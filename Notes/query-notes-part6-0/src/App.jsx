1a9a9075d2150f3578b401161708dcd5db2e3db6
hotaru <huyuchen0810@gmail.com>
2024-05-31 17:17:31 +0900

    update query-notes

--- Notes/query-notes-part6-0/src/App.jsx
+++ Notes/query-notes-part6-0/src/App.jsx
@@ -1,21 +1,45 @@
-import { useQuery } from "@tanstack/react-query"
-import { getNotes } from "./requests"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { getNotes, createNote, updateNote} from "./requests"
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation({
+  mutationFn: createNote,
+  onSuccess: (newNote) => {
+    // queryClient.invalidateQueries({queryKey: ['notes']})
+    const notes = queryClient.getQueryData({queryKey: ['notes']}) // synchronous fuc, get an exxisting query's cached data.
+    queryClient.setQueryData(['notes'], notes.concat(newNote)) // syc fuc, update a query's cached data.
+}
+})
+const updateNoteMutation = useMutation({
+  mutationFn: updateNote,
+  onSuccess: (updatedNote) => {
+    queryClient.invalidateQueries({queryKey: ['notes']})
+    // const notes = queryClient.getQueryData(['notes'])
+    // const changedOne = notes.find(n => n.id === updatedNote.id)
+    // queryClient.setQueryData(['notes'], changedOne)
+
+  }
+})
+ 
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
     console.log(content)
+    newNoteMutation.mutate({content, important: true})
   }
 
   const toggleImportance = (note) => {
     console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({...note, important: !note.important})
   }
 
   const result = useQuery({
     queryKey: ['notes'],
-    queryFn: getNotes
+    queryFn: getNotes,
+    refetchOnWindowFocus : false
   })
   console.log(JSON.parse(JSON.stringify(result)))
   
--- Notes/query-notes-part6-0/src/requests.jsx
+++ Notes/query-notes-part6-0/src/requests.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
-const baseUrl = axios.get('http://localhost:3001/notes')
+const baseUrl = 'http://localhost:3001/notes'
 
-export const getNotes = () => axios.get('http://localhost:3001/notes').then(res => res.data)
+export const getNotes = () => axios.get(baseUrl).then(res => res.data)
 
-export const createNote = (newNote) => axios.post(baseUrl, newNote).then(res => res. data)
+export const createNote = (newNote) => axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateNote = (updatedNote) => axios.put(`${baseUrl}/${updatedNote.id}`, updatedNote).then(res => res.data)
