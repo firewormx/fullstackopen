43ab466c5884b679269025c14dd6628b3df70c4a
hotaru <huyuchen0810@gmail.com>
2024-06-17 18:42:02 +0900

    update webpack/build

--- webpack/src/App.js
+++ webpack/src/App.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React , {useState, useEffect} from "react"
+import axios from "axios"
+import './index.css'
+
+const useNotes = (url) => {
+const [notes, setNotes] = useState([])
+useEffect(() => {
+   axios.get(url).then(response => setNotes(response.data))
+}, [url])
+return notes
+}
 
 const App = () => {
+
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState([])
+    // const url = 'https://notes2023.fly.dev/api/notes'
+    // const notes = useNotes(url)
+    const notes = useNotes(BACKEND_URL)
+
+    const handleClick = () => {
+     setCounter(counter + 1)
+     setValues(values.concat(counter))
+    }
     return (<>
-        <h1>Build</h1>
-        <div>hello webpack</div>
+        <h1 className="title">Build</h1>
+        <div className="container">
+            hello webpack {counter} clicks
+            <button onClick ={handleClick}>press</button>
+            <div>{notes.length} notes on server {BACKEND_URL}</div>
+        </div>
         </>
     )
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,22 +1,48 @@
 const path = require('path') // nodeJS module manipulate file path
+const webpack = require('webpack')
 
-const config = {
- entry:'./src/index.js',
+const config = (env, argv) => {
+    console.log('argv', argv.mode)
+
+    const backend_url = argv.mode === 'production'
+    ? 'https://notes2023.fly.dev/api/notes'
+    : 'http://localhost:3001/notes'
+
+    return {
+    entry:'./src/index.js',
     output:{
         path: path.resolve(__dirname, 'build'),// path.resolve() method resolves a sequence of paths or path segments into an absolute path
         filename: 'main.js'
     },
+    devServer: {
+     static: path.resolve(__dirname, 'build'),
+     compress: true,
+     port: 3000
+    },
+    devtool:'source-map',//map errors that occur during the execution of the bundle to the corresponding part in the original source code.
     module: {
         rules: [
           {
-            test: /\.js$/,
+            test: /\.js$/,// test property specifies the loader is for files that have names ending with .js.
             loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react'],
+            options: {//Babel presets can act as sharable set of Babel plugins and/or config options.
+              presets: ['@babel/preset-env','@babel/preset-react'],
             },
           },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          }
         ],
       },
+      plugins: [
+        new webpack.DefinePlugin({//use webpack's DefinePlugin for defining global default constants that can be used in the bundled code.
+          BACKEND_URL: JSON.stringify(backend_url)//define a new global constant BACKEND_URL that gets a different value depending on the environment that the code is being bundled for:
+
+
+        })
+      ]
+}
 }
 
 
