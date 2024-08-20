5d8abe5a9874571f1f3d5fbb290aab2407f65387
hotaru <huyuchen0810@gmail.com>
2024-08-20 19:05:30 +0900

    update apollo client

--- Apollo Client/apollo client/src/queries.js
+++ Apollo Client/apollo client/src/queries.js
@@ -1,26 +1,30 @@
 import { gql } from "@apollo/client";
 
+const PERSON_DETAILS = gql`
+fragment PersonDetails on Person {
+  name
+  phone 
+  id
+  address{
+    street
+    city 
+  }
+}
+`
 export const ALL_PERSONS = gql`
 query {
   allPersons{
-    name
-    phone
-    id
+...PersonDetails
   }
 }`
 
 export const FIND_PERSON = gql`
 query findPersonByName($nameToSearch: String!) {
   findPerson(name: $nameToSearch) {
-    name
-    phone
-    id
-    address {
-      street
-      city
-    }
+  ...PersonDetails
   }
 }
+${PERSON_DETAILS}
 `
 
 export const  CREATE_PERSON = gql`
