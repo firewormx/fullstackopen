34deafb73593cbb54690e916ed3972b1dc7bc54f
hotaru <huyuchen0810@gmail.com>
2024-08-21 19:10:11 +0900

    update apollo

--- Apollo/index.js
+++ Apollo/index.js
@@ -1,15 +1,21 @@
 const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
-const {v1: uuid} = require('uuid')
-const {GraphQLError} = require('graphql')
-const jwt = require('jsonwebtoken')
-const {typeDefs} = require('./schema')
-const {resolvers} = require('./resolvers')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
 
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
+const jwt = require('jsonwebtoken')
 const mongoose =require('mongoose')
 mongoose.set('strictQuery', false)
-const Person = require('./models/person')
+
 const User = require('./models/user')
+
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+
 require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI
@@ -23,47 +29,41 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-//from Parameters of a resolver...
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  },
-]
+//setup is now within an async func
+const start = async() => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({typeDefs, resolvers}),
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
+  })
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async({req, res}) => {
-const auth = req ? req.headers.authorization : null
-if(auth && auth.startsWith('Bearer ')){
-  const decodedToken = jwt.verify(
-    auth.substring(7), process.env.JWT_SECRET
+  await server.start()
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async({req, res}) => {
+        const auth = req ? req.headers.authorization : null
+        if(auth && auth.startsWith('Bearer ')){
+          const decodedToken = jwt.verify(
+            auth.substring(7), process.env.JWT_SECRET
+          )
+          const currentUser = await User.findById(decodedToken.id).populate('friends')
+          return {currentUser}
+        }
+          },
+    })
   )
-  const currentUser = await User.findById(decodedToken.id).populate('friends')
-  return {currentUser}
+
+  const PORT = 4000
+  
+  httpServer.listen(PORT, () => 
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  )
 }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+
+start()
