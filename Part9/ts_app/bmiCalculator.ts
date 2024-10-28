2ed7e02c1a24997f9877e07c0ac0ae05c4f5b08b
firewormx <huyuchen0810@gmail.com>
2024-10-28 19:37:52 +0900

    update Exercises 9.3

--- Part9/ts_app/bmiCalculator.ts
+++ Part9/ts_app/bmiCalculator.ts
@@ -1,4 +1,25 @@
-function calculateBmi (height: number, weight:number) : string{
+type PrintText = 'Underweight'|'Normal range'|'Over range'|'Obese'
+
+type multiplyValues = {
+    value1: number,
+    value2: number
+}
+
+const parseArguments = (args: string[]): multiplyValues => {
+if(args.length < 4) throw new Error('not enough arguments')
+if(args.length > 4) throw new Error('too many arguments')
+
+if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+    return {
+        value1: Number(args[2]),
+        value2: Number(args[3])
+    }
+}else{
+    throw new Error('Provided values were not numbers!')
+}
+}
+
+function calculateBmi (height: number, weight:number) : PrintText{
 const heightM = height / 100
 const bmi = weight / (heightM * heightM)
 if(bmi <=18.5){
@@ -11,4 +32,21 @@ return 'Over range'
     return 'Obese'
 }
 }
-console.log(calculateBmi(180, 74))
+try{
+    const {value1, value2} = parseArguments(process.argv)
+    calculateBmi(value1, value2)
+}catch(error: unknown){
+    let errorMessage = 'Something error happened.'
+    if(error instanceof Error){
+    errorMessage +=` Error: ` + errorMessage
+}
+console.log(errorMessage)
+}
+
+// npm run calculateBmi 180 74
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
+
+// console.log(calculateBmi(180, 74))
+console.log(calculateBmi(height, weight))
+console.log(process.argv)
--- Part9/ts_app/exerciseCalculator.ts
+++ Part9/ts_app/exerciseCalculator.ts
@@ -10,9 +10,54 @@ interface ResultObj{
         average: number
       }
 
-type DailyHours = [number, number, number, number, number, number, number]
+type DailyHours = number[]
 
-const calculateExercises = (dailyHrs: DailyHours, target: number): ResultObj => {
+type MultipleValues = {
+    value1: number,
+    value2: number,
+    value3: number,
+    value4: number,
+    value5: number,
+    value6: number,
+    value7: number,
+    value8: number,
+    value9: number,
+    value10: number
+}
+
+const parseArgs = (args: string[]): MultipleValues  => {
+if(args.length < 11) throw new Error('not enough arguments')
+if(args.length > 11) throw new Error('too many arguments')
+if(
+!isNaN(Number(args[2])) && 
+!isNaN(Number(args[3])) && 
+!isNaN(Number(args[4])) &&
+!isNaN(Number(args[5])) &&
+!isNaN(Number(args[6])) &&
+!isNaN(Number(args[7])) &&
+!isNaN(Number(args[8])) &&
+!isNaN(Number(args[9])) &&
+!isNaN(Number(args[10])) &&
+!isNaN(Number(args[11]))
+){
+return {
+    value1: Number(args[2]),
+    value2: Number(args[3]),
+    value3: Number(args[4]), 
+    value4: Number(args[5]), 
+    value5: Number(args[6]), 
+    value6: Number(args[7]), 
+    value7: Number(args[8]), 
+    value8: Number(args[9]), 
+    value9: Number(args[10]),
+    value10: Number(args[11])
+}
+}else{
+    throw new Error('Provided values are not numbers!')
+}
+}
+
+const calculateExercises = (target: number, dailyHrs: DailyHours): ResultObj => {
     const periodLength = dailyHrs.length;
     const trainingDays = dailyHrs.filter(hr => hr !== 0).length
     const average = dailyHrs.reduce((current, total)=>  current + total, 0) / dailyHrs.length
@@ -35,10 +80,39 @@ return {
     target,
     average
 }
-
     }
 
+try{
+console.log(parseArgs(process.argv))
+const {value1, value2, value3, value4, value5, value6,
+    value7, value8, value9, value10 }= parseArgs(process.argv)
 
+// const arrayValues = [value2, value3, value4, value5, value6, value7, value8, value9, value10]
+calculateExercises(value1, [value2, value3, value4, value5, value6, value7, value8, value9, value10])
 
+}catch(error:unknown){
+let errorMessage = 'something went wrong.'
+if(error instanceof Error){
+errorMessage += ' Error: ' + errorMessage
+}
+console.log(errorMessage)
+}
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0,3, 1], 2))
+const target: number = Number(process.env[2])
+
+const value2: number = Number(process.argv[3])
+const value3 : number = Number(process.argv[4])
+const value4 : number = Number(process.argv[5])
+const value5 : number = Number(process.argv[6])
+const value6 : number = Number(process.argv[7])
+const value7 : number = Number(process.argv[8])
+const value8 : number = Number(process.argv[9])
+const value9 : number = Number(process.argv[10])
+const value10 : number = Number(process.argv[11])
+
+const dailyHrs = [value2, value3, value4, value5, value6, value7, value8, value9, value10]
+
+console.log(calculateExercises(target, dailyHrs))
+
+console.log(calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]))
+console.log(calculateExercises(2, [1, 0, 2, 4.5, 0, 3, 1, 0, 4]))
