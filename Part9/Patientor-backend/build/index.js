8601e886d31b9bfe962d423678453683573945cd
firewormx <huyuchen0810@gmail.com>
2024-11-08 18:48:58 +0900

    update Part9 - 9.9 Patientor backend

--- Part9/Patientor-backend/build/index.js
+++ Part9/Patientor-backend/build/index.js
@@ -4,12 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.static('dist'));
 app.use(express_1.default.json());
-const PORT = 3000;
+const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
+});
+app.get('/api/patients', (_req, res) => {
+    console.log('check patients info here');
+    res.send('patients info');
 });
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- Part9/Patientor-backend/index.ts
+++ Part9/Patientor-backend/index.ts
@@ -1,13 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json());
 
-const PORT = 3000;
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+
+app.get('/api/patients', (_req, res) => {
+  console.log('check patients info here');
+  res.send('patients info')
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
