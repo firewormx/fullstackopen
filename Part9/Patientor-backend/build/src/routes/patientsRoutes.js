bb9dd75d80a68eac8dec70612f94d4cf2c63bf98
firewormx <huyuchen0810@gmail.com>
2024-11-25 18:47:45 +0900

    update Patientor-backend

--- Part9/Patientor-backend/build/src/routes/patientsRoutes.js
+++ Part9/Patientor-backend/build/src/routes/patientsRoutes.js
@@ -6,22 +6,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const patientsService_1 = __importDefault(require("../services/patientsService"));
 const utils_1 = require("../utils");
+const zod_1 = require("zod");
 const router = express_1.default.Router();
 router.get('/', (_req, res) => {
     res.send(patientsService_1.default.getNonSensitiveInfo());
 });
-router.post('/', (req, res) => {
+const newPatientParser = (req, _res, next) => {
     try {
-        const newPatientEntry = (0, utils_1.toNewPatientEntry)(req.body);
-        const addedPatient = patientsService_1.default.postPatient(newPatientEntry);
-        res.json(addedPatient);
+        utils_1.NewPatientSchema.parse(req.body);
+        next();
     }
     catch (error) {
-        let errorMessage = 'something wrong happened.';
-        if (error instanceof Error) {
-            errorMessage += 'Error: ' + error.message;
-        }
-        res.status(400).send(errorMessage);
+        next(error);
     }
+};
+const errorMiddleware = (error, _req, res, next) => {
+    if (error instanceof zod_1.z.ZodError) {
+        res.status(400).send({ error: error.issues });
+    }
+    else {
+        res.status(400).send({ error: 'unknown error' });
+        next(error);
+    }
+};
+router.post('/', newPatientParser, (req, res) => {
+    // const newPatientEntry = NewPatientSchema.parse(req.body)
+    const addedPatient = patientsService_1.default.postPatient(req.body);
+    res.json(addedPatient);
 });
+router.use(errorMiddleware);
 exports.default = router;
--- Part9/Patientor-backend/build/src/utils.js
+++ Part9/Patientor-backend/build/src/utils.js
@@ -1,64 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.toNewPatientEntry = void 0;
+exports.toNewPatientEntry = exports.NewPatientSchema = void 0;
 const types_1 = require("./types");
-const isString = (text) => {
-    return typeof text == 'string' || text instanceof String;
-};
-const parseName = (name) => {
-    if (!isString(name) || !name) {
-        throw new Error('Incorrect name type or missing field ' + name);
-    }
-    return name;
-};
-const isDate = (birth) => {
-    return Boolean(Date.parse(birth));
-};
-const parseDate = (birth) => {
-    if (!isString(birth) || !birth || !isDate(birth)) {
-        throw new Error('Incorrect birth type or missing field. ' + birth);
-    }
-    return birth;
-};
-const isGender = (param) => {
-    return Object.values(types_1.Gender).map(g => g.toString()).includes(param);
-};
-const parseGender = (gender) => {
-    if (!gender || !isString(gender) || !isGender(gender)) {
-        throw new Error('Incorrect gender type provided or missing field ' + gender);
-    }
-    return gender;
-};
-const parseOccupation = (occupation) => {
-    if (!occupation || !isString(occupation)) {
-        throw new Error('Incorrect occupation type or missing field ' + occupation);
-    }
-    return occupation;
-};
-const parseSsn = (ssn) => {
-    if (!isString(ssn) || !ssn) {
-        throw new Error('Incorrect occupation type or missing field ' + ssn);
-    }
-    return ssn;
-};
+const zod_1 = require("zod");
+exports.NewPatientSchema = zod_1.z.object({
+    name: zod_1.z.string(),
+    dateOfBirth: zod_1.z.string().date(),
+    gender: zod_1.z.nativeEnum(types_1.Gender),
+    occupation: zod_1.z.string(),
+    ssn: zod_1.z.string().optional()
+});
 const toNewPatientEntry = (object) => {
-    if (!object || typeof object !== 'object') {
-        throw new Error('Incorrect data');
-    }
-    if ('name' in object &&
-        'dateOfBirth' in object &&
-        'gender' in object &&
-        'occupation' in object &&
-        'ssn' in object) {
-        const newPatient = {
-            name: parseName(object.name),
-            dateOfBirth: parseDate(object.dateOfBirth),
-            gender: parseGender(object.gender),
-            occupation: parseOccupation(object.occupation),
-            ssn: parseSsn(object.ssn)
-        };
-        return newPatient;
-    }
-    throw new Error('Incorrect data or missing fields');
+    return exports.NewPatientSchema.parse(object);
 };
 exports.toNewPatientEntry = toNewPatientEntry;
