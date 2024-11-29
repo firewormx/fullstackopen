67122d91177d7b5b9e75dbe24cb66d4542a15ed0
firewormx <huyuchen0810@gmail.com>
2024-11-29 16:46:19 +0900

    update Part9 -9.22
    9.22: Patientor, step2
    Create a page for showing a patient's full information in the frontend.

--- Part9/patientor-main/src/components/SpecailPatientEntry.tsx
+++ Part9/patientor-main/src/components/SpecailPatientEntry.tsx
@@ -1,48 +1,35 @@
 import { Patient } from "../types"
-import patientService from '../services/patients'
-
+import { useParams } from "react-router-dom"
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+import Man3Icon from '@mui/icons-material/Man3';
 interface Props{
     patients:Patient[],
 }
 
 const SpecialEntry= ({patients}: Props) => {
-return <div>
-    {patients.map(p => 
-    <div key={p.id}>
-    <h2>{p.name}</h2>&nbsp;{p.gender}
-  <p>ssn:&nbsp; {p.ssn}</p>
-  <p>occupation:&nbsp; {p.occupation}</p>
-  </div>)}
+  const id = useParams().id
+  const specailPatient = patients.find(p => p.id === id)
+
+  const genderPart = () => {
+    if(specailPatient?.gender === 'female'){
+  return <FemaleIcon />
+    }
+    if(specailPatient?.gender === 'male'){
+      return <MaleIcon />
+    }else{
+ return <Man3Icon />
+    }
+  }
+
+return (
+  <div key={specailPatient?.id}>
+  <h2>{specailPatient?.name} {genderPart()}</h2>
+
+<p>ssn:&nbsp; {specailPatient?.ssn}</p>
+<p>occupation:&nbsp; {specailPatient?.occupation}</p>
 </div>
+)
 }
-// const getSpecialEntry = async(id: string) => {
-//     const [error, setError] = useState('')
-//     try{ 
-//         const specailPatient = await patientService.getSpecialPatient(id)
-//         return (
-//           <div>
-//               <h2>{specailPatient.name}</h2>&nbsp;&nbsp;{specailPatient.gender}
-//             <p>ssn:&nbsp; {specailPatient.ssn}</p>
-//             <p>occupation:&nbsp; {specailPatient.occupation}</p>
-//             </div>
-//     )
-//       }catch(e: unknown){
-//     if(axios.isAxiosError(e)){
-//       if(e?.response?.data && typeof e?.response?.data === 'string'){
-//         const message = e.response.data.replace('Something went wrong. Error: ', '');
-//         console.error(message);
-//         setError(message);
-//       }else{
-//         setError("Unrecognized axios error");
-//       }}else{
-//         console.error("Unknown error", e);
-//         setError("Unknown error");
-//       }
-//     }
-// }
-// const SpecialEntry= (id: string) => {
-// return <getSpecialEntry />
-    
-//     }
   
 export default SpecialEntry
