30d330f108dd1029da941fea650b22f152d172b7
firewormx <huyuchen0810@gmail.com>
2025-02-17 21:32:16 +0900

    update port of Patientor

--- Part9/Patientor-backend/src/index.ts
+++ Part9/Patientor-backend/src/index.ts
@@ -21,7 +21,7 @@ app.use('/api/diagnose', diagnoseRouter)
 
 app.use('/api/patients', patientsRouter)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} from .env ${process.env.PORT}`);
 });
