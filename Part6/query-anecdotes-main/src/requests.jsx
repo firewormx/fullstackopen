82944d1e32a3e8088bdb05c82e940511828f4b2f
firewormx <huyuchen0810@gmail.com>
2025-02-19 22:38:04 +0900

    Update requests.jsx

--- Part6/query-anecdotes-main/src/requests.jsx
+++ Part6/query-anecdotes-main/src/requests.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = 'http://localhost:3002/anecdotes'
 
 export const getAll = () => axios.get(baseUrl).then(res => res.data)
 
