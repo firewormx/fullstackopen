920cd0cb864bb98c1bfa210acea04a3f72c2394f
firewormx <huyuchen0810@gmail.com>
2025-02-19 23:50:53 +0900

    Update vite.config.js

--- Part6/redux-anecdotes-main/vite.config.js
+++ Part6/redux-anecdotes-main/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002, 
-    host: '0.0.0.0'
+    port: 3002,
+    host: '0.0.0.0',
+    allowedHosts: ['anecdotes-nrvw.onrender.com'],
   },
 })
