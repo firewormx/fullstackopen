479086a3fc9ac46caca8c4c7447627f8e46c8bf7
firewormx <huyuchen0810@gmail.com>
2025-02-20 20:47:13 +0900

    update Apollo-phonebook

--- Apollo Client/apollo client/vite.config.js
+++ Apollo Client/apollo client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/': {
+        target: 'https://phonebook-wueg.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
